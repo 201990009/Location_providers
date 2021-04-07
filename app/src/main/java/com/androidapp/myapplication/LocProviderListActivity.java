b874e9d5830643c33f5b3835c59fee5fa02119a0
sardor <mirzahmedovs266@gmail.com>
2021-04-07 23:56:45 +0900

    added button (completed)

--- app/src/main/java/com/androidapp/myapplication/LocProviderListActivity.java
+++ app/src/main/java/com/androidapp/myapplication/LocProviderListActivity.java
@@ -3,6 +3,7 @@ package com.androidapp.myapplication;
 import android.app.Activity;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -20,30 +21,32 @@ public class LocProviderListActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         mTextView = findViewById(R.id.txtOutput);
-        locMgr = (LocationManager)getSystemService(LOCATION_SERVICE);
         mButton = findViewById(R.id.button);
 
-        mButton.setOnClickListener(new View.onClickListener(){
+        locMgr = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locProviders = locMgr.getAllProviders();
+
+        //locMgr = (LocationManager) getSystemService(LOCATION_SERVICE);
+        //locProviders = locMgr.getAllProviders();
+
+        mButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                locMgr = (LocationManager)get.SystemService(LOCATION_SERVICE);
-                locProviders = locMgr.getAllProviders();
+            public void onClick(View v) {
+
+
+
                 String s = "";
+                for (int i = 0; i < locProviders.size(); i++){
+                    s += "Loc.Provider: " + locProviders.get(i) + "\n"
+                            + "Status" + locMgr.isProviderEnabled(locProviders.get(i)) + "\n\n";
+                }
+                mTextView.setText(s);
 
             }
+        });
+
 
         }
-
-
-
-
-        /*for(int i = 0; i < locProviders.size(); i++){
-            s += "Loc. Provider " + locProviders.get(i) + "\n"
-                    + "Status: " + locMgr.isProviderEnabled(locProviders.get(i)) + "\n\n";
-        }*/
-        //Enhanced for Loop
-                mTextView.setText(s);
-    }
 
 
 }

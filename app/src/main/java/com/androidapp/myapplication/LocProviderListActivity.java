56a3a6f03e791d34b9b7f32dd943426f66d3937f
sardor <mirzahmedovs266@gmail.com>
2021-04-07 23:45:45 +0900

    added button

--- app/src/main/java/com/androidapp/myapplication/LocProviderListActivity.java
+++ app/src/main/java/com/androidapp/myapplication/LocProviderListActivity.java
@@ -3,6 +3,7 @@ package com.androidapp.myapplication;
 import android.app.Activity;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
@@ -11,6 +12,7 @@ public class LocProviderListActivity extends Activity {
     TextView mTextView;
     LocationManager locMgr;
     List<String> locProviders;
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstantceState){
@@ -19,15 +21,28 @@ public class LocProviderListActivity extends Activity {
 
         mTextView = findViewById(R.id.txtOutput);
         locMgr = (LocationManager)getSystemService(LOCATION_SERVICE);
+        mButton = findViewById(R.id.button);
 
-        locProviders = locMgr.getAllProviders();
+        mButton.setOnClickListener(new View.onClickListener(){
+            @Override
+            public void onClick(View v){
+                locMgr = (LocationManager)get.SystemService(LOCATION_SERVICE);
+                locProviders = locMgr.getAllProviders();
+                String s = "";
 
-        String s = "";
-        for(int i = 0; i < locProviders.size(); i++){
+            }
+
+        }
+
+
+
+
+        /*for(int i = 0; i < locProviders.size(); i++){
             s += "Loc. Provider " + locProviders.get(i) + "\n"
                     + "Status: " + locMgr.isProviderEnabled(locProviders.get(i)) + "\n\n";
-        }
-        mTextView.setText(s);
+        }*/
+        //Enhanced for Loop
+                mTextView.setText(s);
     }
 
 
